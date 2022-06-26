5d97ec2728394b9973c6f486d791d12d07001100
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-26 18:31:10 +0200

    add empty form to contact page; update cart icon

--- src/contact.js
+++ src/contact.js
@@ -3,17 +3,25 @@ export const loadContactPage = () => {
 
   const contactContainer = document.createElement('div');
   const contactHeading = document.createElement('h1');
+  const contactParagraph = document.createElement('p');
   const iframeMap = document.createElement('iframe');
+  const contactDiv = document.createElement('div');
+  const contactForm = document.createElement('form');
 
   contactContainer.classList.add('contact');
   contactHeading.classList.add('page-heading');
   iframeMap.classList.add('map');
+  contactDiv.classList.add('contact-container');
+  contactForm.classList.add('contact-form');
 
   contactHeading.textContent = 'Contact Us';
+  contactParagraph.textContent =
+    "We’d love to hear from you and we'll do our best to get back to you as soon as possible.";
 
   iframeMap.src =
     'https://www.openstreetmap.org/export/embed.html?bbox=-1.3126891851425173%2C53.69312690948469%2C-1.3092076778411867%2C53.69459593405764&amp;layer=mapnik';
 
-  contactContainer.append(contactHeading, iframeMap);
+  contactDiv.append(contactForm, iframeMap);
+  contactContainer.append(contactHeading, contactParagraph, contactDiv);
   contentDiv.append(contactContainer);
 };
